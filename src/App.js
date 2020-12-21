0e2fbce108445e5528c8169ecacb6b6d7ac53703
sedx876 <swatkins876@gmail.com>
2020-12-21 06:16:42 -0500

    main routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { Route, Switch} from 'react-router-dom'
 import "bootswatch/dist/cyborg/bootstrap.min.css"
 import Home from './components/Home'
 import Navbar from './components/Navbar'
@@ -15,14 +15,16 @@ import Films from './components/Films'
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      {/* <About /> */}
-      {/* <News /> */}
-      {/* <BehindTheScenes/> */}
-      {/* <Development /> */}
-      {/* <Films /> */}
-      <Footer />
+      <Navbar/>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
+        <Route exact path='/bts' component={BehindTheScenes}/>
+        <Route exact path='/news' component={News}/>
+        <Route exact path='/development' component={Development}/>
+        <Route exact path='/films' component={Films}/>
+      </Switch>
+      <Footer /> 
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <App />,
+  <Router><App /></Router>,
   document.getElementById('root')
 );
 
