b1c31d203a8c5a393c334774bbd186c1307f8da7
sedx876 <swatkins876@gmail.com>
2020-12-21 06:58:08 -0500

    navbar navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,17 +9,72 @@ const Navbar = () => {
  
 
 <div class="navbar">
-<a class="logo" href="#"><img className='cameo' src={cameo}/> Ghost Walk</a>
+  <NavLink className="logo" exact 
+    activeClassName='active' 
+    to='/'>
+    <img className='cameo' src={cameo} alt='cameo'/> 
+    Ghost Walk
+  </NavLink>
+
 <ul class="nav">
-  <li><a href="#home">About</a></li>
-  <br></br>
-  <li><a href="#about">Behind the Scenes</a></li>
+  <li>
+    <NavLink className='about' 
+    exact activeClassName='active' 
+    to='/about'>
+      About
+    </NavLink>
+  </li>
+
+  <li>
+    <NavLink className='bts'
+    exact activeClassName='active'
+    to='/bts'>
+      Behind the Scenes
+    </NavLink>
+  </li>
+
   <li><a href="#portfolio">Press</a></li>
-  <li><a href="#services">Films</a></li>
-  <li><a href="#contact">Development</a></li>
-  <li><a href="#contact">News</a></li>
-  <li><a href="#contact">More</a></li>
-  <li><a href="#contact">Contact</a></li>
+
+  <li>
+    <NavLink className='films' 
+    exact activeClassName='active' 
+    to='/films'>
+      Films
+    </NavLink>
+  </li>
+
+  <li>
+    <NavLink className='development' 
+    exact activeClassName='active' 
+    to='/development'>
+      Development
+    </NavLink>
+  </li>
+
+  <li>
+    <NavLink className='news' 
+    exact activeClassName='active' 
+    to='/news'>
+      News
+    </NavLink>
+  </li>
+
+  <li>
+    <NavLink className='more' 
+    exact activeClassName='active' 
+    to='/more'>
+      More
+    </NavLink>
+  </li>
+
+  <li>
+    <NavLink className='contact' 
+    exact activeClassName='active' 
+    to='/contact'>
+      Contact
+    </NavLink>
+  </li>
+  
 </ul>
 </div>
   )
