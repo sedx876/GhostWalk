ed898545881d4d4117ab42132fc469777f7c28d5
sedx876 <swatkins876@gmail.com>
2020-12-22 06:56:10 -0500

    work on development.js

--- src/components/Development.js
+++ src/components/Development.js
@@ -10,13 +10,13 @@ const Development = () => {
   return (
     <>
       <h2>Development</h2>
-      <div class="card border-secondary mb-3">
-        <div class="card-header">DEAD SLATE</div>
-        <div class="card-body">
-          <h5 class="card-title">Director: Staci Layne Wilson</h5>
-          <h5 class="card-title">Screenwriter: Kerry Fleming</h5>
-          <h5 class="card-title">Story By: Victoria & Tony Vardon</h5>
-          <h5 class="card-title">Genre: Horror</h5>
+      <div>
+        <h3>DEAD SLATE</h3>
+        <div>
+          <h5>Director: Staci Layne Wilson</h5>
+          <h5>Screenwriter: Kerry Fleming</h5>
+          <h5>Story By: Victoria & Tony Vardon</h5>
+          <h5>Genre: Horror</h5>
             <img className="DeadSlate" 
             alt="DeadSlate" 
             src={DeadSlate}/>
@@ -29,34 +29,34 @@ const Development = () => {
 
       <h2>Coming Soon...</h2>
 
-      <div class="card border-secondary mb-3">
-        <div class="card-header">Future Imperfect</div>
-        <div class="card-body">
-          <h5 class="card-title">Shooting Begins: June 2020</h5>
+      <div>
+        <h3>Future Imperfect</h3>
+        <div>
+          <h5>Shooting Begins: June 2020</h5>
             <img className="FutureImperfect" 
             alt="FutureImperfect" 
             src={FutureImperfect}/> 
       </div>
       </div>
-
-      <div class="card border-secondary mb-3">
-        <div class="card-header">The End Was Then</div>
-        <div class="card-body">
-        <h5 class="card-title">Director: Rocky Karlage</h5>
-        <h5 class="card-title">Screenwriter: Rocky Karlage</h5>
+        <br/><br/><br/>
+      <div>
+        <h3>The End Was Then</h3>
+        <div>
+        <h5>Director: Rocky Karlage</h5>
+        <h5>Screenwriter: Rocky Karlage</h5>
           <h5 class="card-title">Shooting Begins: May 2019</h5>
             <img className="TheEndWas" 
             alt="TheEndWas" 
             src={TheEndWas}/> 
       </div>
       </div>
-
-      <div class="card border-secondary mb-3">
-        <div class="card-header">The Harvest Ball</div>
-        <div class="card-body">
-        <h5 class="card-title">Director: Rocky Karlage</h5>
-        <h5 class="card-title">Screenwriter: Rocky Karlage</h5>
-          <h5 class="card-title">Shooting Begins: TBA</h5>
+        <br/><br/><br/>
+      <div>
+        <h3>The Harvest Ball</h3>
+        <div>
+        <h5>Director: Rocky Karlage</h5>
+        <h5>Screenwriter: Rocky Karlage</h5>
+          <h5>Shooting Begins: TBA</h5>
             <img className="HarvestBall" 
             alt="HarvestBall" 
             src={HarvestBall}/> 
